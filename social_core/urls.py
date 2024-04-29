4716dc09767682330556c9a10b20f5b1169dcddf
nidhi tagline <nidhig.tagline@gmail.com>
2024-04-29 15:26:13 +0530

    Added requirements.txt and applied pre-commit

--- social_core/urls.py
+++ social_core/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
@@ -26,7 +27,7 @@ from drf_yasg import openapi
 schema_view = get_schema_view(
     openapi.Info(
         title="SocialMedia API",
-        default_version='v1',
+        default_version="v1",
         description="Test description",
     ),
     public=True,
@@ -34,9 +35,13 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("admin/", admin.site.urls),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
