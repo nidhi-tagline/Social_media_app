c3c6f87eaece7c747ffeb5b894e2f93920986e3e
nidhi tagline <nidhig.tagline@gmail.com>
2024-05-01 17:04:28 +0530

    Create custom user model and profile model

--- apis/admin.py
+++ apis/admin.py
@@ -1,10 +1,2 @@
 from django.contrib import admin
 
-from .models import Comment, Friend, FriendRequest, Post, Profile, User
-
-admin.site.register(User)
-admin.site.register(Profile)
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Friend)
-admin.site.register(FriendRequest)
